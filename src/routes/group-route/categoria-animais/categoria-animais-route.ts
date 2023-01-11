5bcbf8d02c373876c9ddbed496526f0d0aac185f
igorfals <igorfeals@gmail.com>
2023-01-11 11:39:48 -0300

    Criando router categoria de animais

--- src/routes/group-route/categoria-animais/categoria-animais-route.ts
+++ src/routes/group-route/categoria-animais/categoria-animais-route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-
+import { CategoriaAnimaisMiddleWare } from '../../../middlewares/categoria-animais'
 
 const router = Router()
+const categoriaanimaisMiddleWare = new CategoriaAnimaisMiddleWare()
 
-
+router.post('/add', categoriaanimaisMiddleWare.setCategoriaanimais)
 
 export default router
