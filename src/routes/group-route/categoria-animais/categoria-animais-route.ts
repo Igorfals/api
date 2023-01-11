1a89002cea228ff53f432bcbdda5ed3b0eb3ad54
igorfals <igorfeals@gmail.com>
2023-01-11 16:26:03 -0300

    Criando delete no routes group-route

--- src/routes/group-route/categoria-animais/categoria-animais-route.ts
+++ src/routes/group-route/categoria-animais/categoria-animais-route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { CategoriaAnimaisMiddleWare } from '../../../middlewares/categoria-animais'
-import { TipoAnimaisMiddleWare } from '../../../middlewares/tipo-animais'
+
 
 const router = Router()
 const categoriaanimaisMiddleWare = new CategoriaAnimaisMiddleWare()
 
 router.post('/add', categoriaanimaisMiddleWare.setCategoriaanimais)
 router.put('/update', categoriaanimaisMiddleWare.updatecategoriaAnimais)
+router.delete('/delete:id', categoriaanimaisMiddleWare.deletecategoriaAnimais)
 export default router
