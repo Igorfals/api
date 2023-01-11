1722bc798192a3f60e2427abec116cb2c88ae4e3
igorfals <igorfeals@gmail.com>
2023-01-11 12:59:55 -0300

    criando o a rota para update em group-route

--- src/routes/group-route/categoria-animais/categoria-animais-route.ts
+++ src/routes/group-route/categoria-animais/categoria-animais-route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { CategoriaAnimaisMiddleWare } from '../../../middlewares/categoria-animais'
+import { TipoAnimaisMiddleWare } from '../../../middlewares/tipo-animais'
 
 const router = Router()
 const categoriaanimaisMiddleWare = new CategoriaAnimaisMiddleWare()
 
 router.post('/add', categoriaanimaisMiddleWare.setCategoriaanimais)
-
+router.put('/update', categoriaanimaisMiddleWare.updatecategoriaAnimais)
 export default router
